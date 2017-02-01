84a4ee21ba2a71894130b347db24df89977fbd57
Yung chieh Tsai <x01ep23i@hotmail.com>
2017-02-01 17:09:43 +0800

    Add order by on SQL

--- db/models/rss.js
+++ db/models/rss.js
@@ -8,11 +8,11 @@ Rss.prototype.read = function(id) {
 }
 
 Rss.prototype.readAll = function(limit = 3, offset = 0) {
-    return pool.query('SELECT * FROM rss LIMIT ' + limit + ' OFFSET ' + offset)
+    return pool.query('SELECT * FROM rss ORDER BY id LIMIT ' + limit + ' OFFSET ' + offset)
 }
 
 Rss.prototype.readByCategoryId = function(categoryId, limit = 3, offset = 0) {
-	return pool.query('SELECT * FROM rss WHERE categoryId = $1 LIMIT ' + limit + ' OFFSET ' + offset, [categoryId]);
+	return pool.query('SELECT * FROM rss WHERE categoryId = $1 ORDER BY id LIMIT ' + limit + ' OFFSET ' + offset, [categoryId]);
 }
 
 Rss.prototype.create = function(entity) {
