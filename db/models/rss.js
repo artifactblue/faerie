e37a9f51d59f73d9ab47792800cc746a4238a5c6
Yung chieh Tsai <x01ep23i@hotmail.com>
2017-02-01 17:50:51 +0800

    Fix bug

--- db/models/rss.js
+++ db/models/rss.js
@@ -12,7 +12,7 @@ Rss.prototype.readAll = function(limit = 3, offset = 0) {
 }
 
 Rss.prototype.readByCategoryId = function(categoryId, limit = 3, offset = 0) {
-	return pool.query('SELECT * FROM rss WHERE categoryId = $1 ORDER BY id LIMIT ' + limit + ' OFFSET ' + offset, [parseInt(categroyId, 10)])
+	return pool.query('SELECT * FROM rss WHERE categoryId = $1 ORDER BY id LIMIT ' + limit + ' OFFSET ' + offset, [parseInt(categoryId, 10)])
 }
 
 Rss.prototype.create = function(entity) {
